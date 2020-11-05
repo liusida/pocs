3b4a4c1c32d6b582d073277b8b7ae3c433448f10
sida <sliu1@uvm.edu>
2020-11-05 09:27:43 -0500

    fix some bugs

--- prototype_0/main.py
+++ prototype_0/main.py
@@ -22,12 +22,13 @@ class Vehicle:
 
     def step(self, action):
         steering, acceleration = action
-        steering *= 0.001 # Definitly need Normalization
-        acceleration *= 1.0
+        steering = steering * 0.1
+        acceleration = acceleration * 10
         self.angle += steering
         self.angle = self.angle % (2 * np.pi)
-        self.velocity += acceleration
-        self.velocity = np.min([self.world.max_velocity, self.velocity])
+        self.velocity = self.world.default_velocity + acceleration
+        if self.velocity < 0:
+            self.velocity = 0
         # update position after updating angle and velocity, the vehicle can respond faster
         self.pos_x += np.sin(self.angle) * self.velocity * self.world.dt
         self.pos_y += np.cos(self.angle) * self.velocity * self.world.dt
@@ -36,13 +37,14 @@ class Vehicle:
         self.pos_y = self.pos_y % self.world.height
 
     def get_obs(self):
-        return [self.pos_x, self.pos_y, self.angle]
+        # Normalized observations (0,1)
+        return [self.pos_x / self.world.width, self.pos_y / self.world.height, self.angle / 2 / np.pi]
 
 
 class World:
     def __init__(self):
         self.dt = 0.1       # delat time, step size
-        self.max_velocity = 100.0
+        self.default_velocity = 100.0
 
         self.dim_obs = 0
         self.dim_action = 0
@@ -52,12 +54,13 @@ class World:
         self.vehicles = []
 
     def init_vehicles(self, num):
+        self.num_vehicles = num
         v = None
         self.vehicles = []
         for i in range(num):
             v = Vehicle(self)
             self.vehicles.append(v)
-        self.dim_obs = 1 + (1 + num) * v.dim_obs
+        self.dim_obs = (1 + num) * v.dim_obs
         self.dim_action = v.dim_action
 
     def step(self, action):
@@ -68,7 +71,7 @@ class World:
     def reset(self):
         for i, vehicle in enumerate(self.vehicles):
             vehicle.reset()
-            vehicle.pos_x = 100 + i * 10
+            vehicle.pos_x = 100 + i * 20
             vehicle.pos_y = 100
         return self.get_obs()
 
@@ -81,7 +84,7 @@ class World:
         all_members = np.array(all_members).flatten()
         ret = []
         for i, vehicle in enumerate(self.vehicles):
-            ret.append(np.concatenate([[i], vehicle.get_obs(), all_members]))
+            ret.append(np.concatenate([vehicle.get_obs(), all_members]))
         return np.array(ret)
 
 
@@ -94,8 +97,8 @@ class Policy:
         self.init_params()
 
     def init_params(self):
-        self.weights = np.random.random([self.dim_obs, self.dim_action])
-        self.bias = np.random.random([self.dim_action])
+        self.weights = np.random.random([self.dim_obs, self.dim_action]) * 2 - 1
+        self.bias = np.random.random([self.dim_action]) * 2 - 1
         print("weights")
         print(self.weights)
         print("bias")
@@ -107,7 +110,7 @@ class Policy:
 
 class Simulation(threading.Thread):
     def run(self):
-        global g_obs
+        global g_obs, g_world
         g_world = World()
         g_world.init_vehicles(10)
 
@@ -125,17 +128,24 @@ class Simulation(threading.Thread):
 # P5 interface
 
 def setup():
-    size(1000, 1000)
+    size(g_world.width, g_world.height)
     no_stroke()
 
 def draw():
-    global g_obs
+    hack_check_window_size()
     background(27, 73, 98)
-    all_vehicles = g_obs[0,4:]
+    all_vehicles = g_obs[0,3:]
     all_vehicles = all_vehicles.reshape([-1,3])
     for v in all_vehicles:
         draw_vehicle( *v )
-    print(all_vehicles)
+    # print(all_vehicles)
+
+def hack_check_window_size():
+    """ I use tile in Linux, so window size changes after it opens. """
+    global g_world
+    if g_world.width != p5.sketch.size[0] and g_world.height != p5.sketch.size[1]:
+        g_world.width = p5.sketch.size[0]
+        g_world.height = p5.sketch.size[1]
 
 def draw_vehicle(pos_x, pos_y, angel):
     p1 = [0, 10]
@@ -145,8 +155,8 @@ def draw_vehicle(pos_x, pos_y, angel):
     p5 = [+1, +5]
     with push_matrix():
         with push_style():
-            translate(pos_x, pos_y)
-            rotate(-angel)
+            translate(pos_x * g_world.width, pos_y * g_world.height)
+            rotate(-angel*2*np.pi)
             scale(1.3)
             fill(Color(136, 177, 112))
             triangle(p1, p2, p3)
@@ -156,6 +166,7 @@ def draw_vehicle(pos_x, pos_y, angel):
 
 if __name__ == "__main__":
     g_obs = None
+    g_world = None
     sim = Simulation()
     sim.start()
     # after start simulation thread, start to draw using p5
