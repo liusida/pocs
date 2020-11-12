b27a82d15cb7b51df06ad3c29050c399cd987f54
sida <sliu1@uvm.edu>
2020-11-12 09:55:08 -0500

    less decorative, a little faster in visualization.

--- prototype_0/utils.py
+++ prototype_0/utils.py
@@ -1,3 +1,13 @@
+from timeit import default_timer as timer
+
 def log(*args):
     if True:
         print(*args)
+
+g_start = 0
+def reset_timer(description=None):
+    global g_start
+    if description is not None:
+        end = timer()
+        print(f"reset_timer: {description} {end-g_start:.03f}")
+    g_start = timer()
