573c629617bd5a7603c83d16f772eb6b8b0a8a36
David Matthews <dmatthe1@uvm.edu>
2020-11-13 16:21:37 -0500

    Adding a macro micro entropy which is the difference btween the two calculations

--- prototype_0/metric.py
+++ prototype_0/metric.py
@@ -73,3 +73,34 @@ class MacroEntropyMetric(EntropyMetric):
             return entropy / (np.log2( 1/self.history_len)* -1) # normalize to 0 to 1.
         else:
             return 0
+
+
+class MacroMicroEntropyMetric(EntropyMetric):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.grid_size= kwargs["grid_size"] if "grid_size" in kwargs else 50
+
+    def get_metric(self):
+        self.update_history()
+        
+        macro_entropy = 0
+        micro_entropy = 0
+        if (self.world_history.shape[1] != 0):
+            world_pos_history = (self.world_history[:, :, :2] * self.grid_size).astype(np.int)
+            vals, counts = np.unique(world_pos_history, return_counts=True, axis=0) # val counts of pos x.
+            macro_entropy = calc_entropy(counts)
+            macro_entropy /= (np.log2( 1/self.history_len)* -1) # normalize to 0 to 1.
+
+
+        total_micro_entropy = 0
+        for v_id in range(self.world_history.shape[1]):
+            # save and bin to ints.
+            row_history = (self.world_history[:, v_id] * self.grid_size).astype(np.int)
+            vals, counts = np.unique(row_history, return_counts=True, axis=0) # val counts of state history of one particle
+            curr_entropy = calc_entropy(counts)
+            curr_entropy /= (np.log2( 1/self.history_len)* -1) # normalize to 0 to 1
+            total_micro_entropy += curr_entropy
+        micro_entropy =  total_micro_entropy / self.world_history.shape[1] # normalize to 0 to 1
+
+        print("micro: %.2f, macro: %.2f"%(micro_entropy, macro_entropy))
+        return micro_entropy - macro_entropy
