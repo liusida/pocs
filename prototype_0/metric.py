aaab67c6a992f51391dba3223a9e4c5606a9c371
David Matthews <dmatthe1@uvm.edu>
2020-11-13 15:06:00 -0500

    micro entropy calculation now includes x,y,vel, theta

--- prototype_0/metric.py
+++ prototype_0/metric.py
@@ -20,14 +20,22 @@ class MicroEntropyMetric:
         if self.world_history.shape[1] != len(self.world.vehicles):
             self.world_history = np.zeros(shape=(self.history_len, len(self.world.vehicles), 4))
             self.history_idx = 0
+        world_width = self.world.width
+        world_height = self.world.height
+        world_size = min(world_width, world_height)
         for v_id in range(self.world_history.shape[1]):
-            self.world_history[self.history_idx, v_id] = [self.world.vehicles[v_id].pos_x, self.world.vehicles[v_id].pos_y,  self.world.vehicles[v_id].velocity,  self.world.vehicles[v_id].angle]
+            # save normalized metrics. between 0 and 1.
+            self.world_history[self.history_idx, v_id] = [self.world.vehicles[v_id].pos_x /world_width,
+                                                            self.world.vehicles[v_id].pos_y/ world_height,
+                                                            self.world.vehicles[v_id].velocity / world_size,
+                                                            self.world.vehicles[v_id].angle / (np.pi * 2)]
         self.history_idx += 1
         self.history_idx %= self.history_len
         
         total_micro_entropy = 0
         for v_id in range(self.world_history.shape[1]):
-            # /10 bins the 1000x1000 world into 100 cells in x and y
-            vals, counts = np.unique((self.world_history[:, v_id, 0]/10).astype(np.int), return_counts=True) # val counts of pos x.
+            # save and bin to ints.
+            row_history = (self.world_history[:, v_id] * 100).astype(np.int)
+            vals, counts = np.unique(row_history, return_counts=True, axis=0) # val counts of pos x.
             total_micro_entropy += stats.entropy(counts)
         return total_micro_entropy
