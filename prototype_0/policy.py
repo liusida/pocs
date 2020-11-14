38350ad23a8a75f49036d52ca2f1761963c45499
sida <sliu1@uvm.edu>
2020-11-13 20:39:59 -0500

    demo of how to use get_action_from_pos_x_pos_y_velocity()

--- prototype_0/policy.py
+++ prototype_0/policy.py
@@ -8,8 +8,19 @@ class Policy:
         self.dim_action = dim_action
         self.num_vehicles = None
 
-    def get_action(self, obs):
+    def get_action_circle(self, obs):
         if self.num_vehicles is None:
             self.num_vehicles = obs.shape[0] - 1
         ret = np.zeros([self.num_vehicles, 2])
+        ret[:,0] = -0.01
+        ret[:,1] = 0.1
+        return ret
+
+    def get_action(self, obs):
+        # demo for get_action_from_pos_x_pos_y_velocity
+        if self.num_vehicles is None:
+            self.num_vehicles = obs.shape[0] - 1
+        ret = np.zeros([self.num_vehicles, 3])
+        ret[:,2] = 0.1
+        ret = self.world.get_action_from_pos_x_pos_y_velocity(ret)
         return ret
