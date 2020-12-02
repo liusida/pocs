aad79e6765c5ae20fbb4d30e23225c40a66c101d
sida <sliu1@uvm.edu>
2020-12-02 18:45:29 -0500

    show venn diagram of two random variables

--- prototype_0/investigate_mi.py
+++ prototype_0/investigate_mi.py
@@ -7,8 +7,11 @@ import pyinform as pyin
 from matplotlib_venn import venn2
 import matplotlib.pyplot as plt
 
+def r(n):
+    """venn2 can't show specified values, so round the values instead"""
+    return round(n,3)
 
-def investigate(x,y):
+def investigate(x,y, title=""):
     """x and y are observations of X and Y"""
     assert x.shape==y.shape, "Can't do mutual information on observations of different length"
 
@@ -31,18 +34,52 @@ def investigate(x,y):
     # I(X;Y)
     MI_xy = Hy - Hy_given_x
 
-    print( f"H(X): {Hx}, H(Y): {Hy}" )
+    print( f"H(X): {Hx}" )
+    print( f"H(Y): {Hy}")
     print( f"H(X,Y): {Hxy}" )
     print( f"H(Y|X): {Hy_given_x}")
+    print( f"H(X|Y): {Hx_given_y}")
     print( f"I(X;Y): {MI_xy}")
     # In short:
     print( f"pyin.mutual_info: {pyin.mutual_info(x,y)}" )
 
-    venn2(subsets = (Hy_given_x, Hx_given_y, MI_xy), set_labels = ("H(X)", "H(Y)", "I(X;Y)"))
-    plt.show()
+    venn2(subsets = (r(Hy_given_x), r(Hx_given_y), r(MI_xy)), set_labels = ("H(X)", "H(Y)", "I(X;Y)"), normalize_to=1)
+    plt.title(f"{title}")
+    plt.savefig(f"investigate_{title}.png")
+    plt.close()
+    return {
+        "H(X)": Hx,
+        "H(Y)": Hy,
+        "H(X,Y)": Hxy,
+        "H(Y|X)": Hy_given_x,
+        "H(X|Y)": Hx_given_y,
+        "I(X;Y)": MI_xy,
+    }
 
-x = [1,1,1,2,2,2]
-y = [1,1,1,2,2,1]
-x = np.array(x)
-y = np.array(y)
-investigate(x,y)
+if __name__ == "__main__":
+        
+    x = [1,1,1,2,2,2,3]
+    y = [1,2,1,2,1,2,3]
+    x = np.array(x)
+    y = np.array(y)
+    # investigate(x,y)
+
+    x = np.random.randint(low=0, high=100, size=[100000])
+    y = np.random.randint(low=0, high=100, size=[100000])
+    print(f"x: {x}")
+    print(f"y: {y}")
+    investigate(x,y,title="100k steps")
+
+    x = np.random.randint(low=0, high=100, size=[1000])
+    y = np.random.randint(low=0, high=100, size=[1000])
+    print(f"x: {x}")
+    print(f"y: {y}")
+    investigate(x,y,title="1k steps")
+
+    x = np.random.randint(low=0, high=100, size=[10])
+    y = np.random.randint(low=0, high=100, size=[10])
+    print(f"x: {x}")
+    print(f"y: {y}")
+    investigate(x,y,title="10 steps")
+
+    
