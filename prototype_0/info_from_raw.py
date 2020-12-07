e5a556cf4517f5668f41c739a1b7d3ee209b18b2
Caitlin Grasso <caitlingrasso@Caitlins-MacBook-Pro-2.local>
2020-12-07 18:40:59 -0500

    minor plotting fix

--- prototype_0/info_from_raw.py
+++ prototype_0/info_from_raw.py
@@ -10,22 +10,6 @@ from investigate_mi import investigate, plot_venn
 
 
 from matplotlib_venn import venn3, venn3_circles
-# plt.figure(figsize=(4,4))
-# v = venn3(subsets=(1, 1, 1, 1, 1, 1, 1), set_labels = ('A', 'B', 'C'))
-# # v.get_patch_by_id('100').set_alpha(1.0)
-# # v.get_patch_by_id('100').set_color('white')
-# # v.get_label_by_id('100').set_text('Unknown')
-# # v.get_label_by_id('A').set_text('Set "A"')
-# # c = venn3_circles(subsets=(1, 1, 1, 1, 1, 1, 1), linestyle='dashed')
-# # c[0].set_lw(1.0)
-# # c[0].set_ls('dotted')
-# plt.title("Sample Venn diagram")
-# plt.annotate('Unknown set', xy=v.get_label_by_id('100').get_position() - np.array([0, 0.05]), xytext=(-70,-70),
-#              ha='center', textcoords='offset points', bbox=dict(boxstyle='round,pad=0.5', fc='gray', alpha=0.1),
-#              arrowprops=dict(arrowstyle='->', connectionstyle='arc3,rad=0.5',color='gray'))
-# plt.show()
-
-# exit()
 
 def load_data(fn):
     with open(fn,'rb') as f:
@@ -55,15 +39,15 @@ for p in policies:
     files.append('{}/{}_10000steps_0seed.p'.format(p, p))
 steps = [10000]
 
-# fig, ax = plt.subplots(len(policies),3, figsize=(10,15))
-fig, ax = plt.subplots(6,len(policies), figsize=(15,13))
+fig, ax = plt.subplots(len(policies),3, figsize=(10,15))
+# fig, ax = plt.subplots(6,len(policies), figsize=(15,13))
 
 for i,f in enumerate(files):
 
     data = load_data(f)
 
-    states = ['x','y','angle','velocity', 'pos', 'angle_velocity']
-    # states = ['velocity']
+    # states = ['x','y','angle','velocity', 'pos', 'angle_velocity']
+    states = ['velocity']
 
     # no moving window - history length is the entire sim
     # comparing two vehicle timeseries only
@@ -73,10 +57,10 @@ for i,f in enumerate(files):
     # granularities = [10, 100, 1000]
 
     for j,bins in enumerate(granularities):
-        # print(policies[i], ":", bins, "bins")
+        print(policies[i], ":", bins, "bins")
 
         for k,state in enumerate(states):
-            print(policies[i], ":", state)
+            # print(policies[i], ":", state)
             Hy_given_x = []
             Hx_given_y = []
             MI_xy = []
@@ -104,13 +88,15 @@ for i,f in enumerate(files):
             MI_xy_avg = np.mean(MI_xy)
 
             # title = "{}, {} steps, {} states, state={}, Average".format(policy, steps[i], bins, state)
-            plot_venn(Hy_given_x_avg, Hx_given_y_avg, MI_xy_avg, ax=ax[k,i], with_numbers=False)
+            # plot_venn(Hy_given_x_avg, Hx_given_y_avg, MI_xy_avg, ax=ax[k,i], with_numbers=False)
+            plot_venn(Hy_given_x_avg, Hx_given_y_avg, MI_xy_avg, ax=ax[i,j], with_numbers=True)
 
 
-# cols = ["{} bins".format(g) for g in granularities]
+cols = ["{} bins".format(g) for g in granularities]
+rows = ['Null 1', 'Null 2 - Random', 'Follow Leader', 'Boids 1', 'Boids 2', 'Random Network 1', 'Random Network 2']
 
-cols = ['Policy', 'Random', 'Follow Leader', 'Boids', 'Simplified Boids', 'Random Network', 'Random Network 2']
-rows = ['x','y','angle','velocity', '(x, y)', '(angle, velocity)']
+# cols = ['Null 1', 'Null 2 - Random', 'Follow Leader', 'Boids 1', 'Boids 2', 'Random Network 1', 'Random Network 2']
+# rows = ['x','y','angle','velocity', '(x, y)', '(angle, velocity)']
 
 for a, col in zip(ax[0], cols):
     a.set_title(col)
@@ -122,5 +108,5 @@ for a, row in zip(ax[:,0], rows):
 
 fig.tight_layout()
 # plt.show()
-plt.savefig('policy_state_VDs_10ksteps_10bins_without_numbers.png', dpi=300)
+plt.savefig('velocity_10ksteps_with_numbers.png', dpi=300)
     
