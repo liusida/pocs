41d20dd1d38fa6bb3d2976fa9bab348b883adf7e
David Matthews <David.Matthews.1@uvm.edu>
2020-12-08 09:59:04 -0500

    more bar plots + temporal line plots

--- prototype_0/analyze_group.py
+++ prototype_0/analyze_group.py
@@ -43,7 +43,7 @@ def investigate(x, y):
     # I(X;Y)
     MI_xy = Hy - Hy_given_x
 
-    return (Hx, Hy, Hxy, Hy_given_x, Hx_given_y, MI_xy, MI_xy/min(Hx, Hy))
+    return (min(Hx, Hy), Hx + Hy ,  Hx, Hy, Hxy, Hy_given_x, Hx_given_y, MI_xy, MI_xy/min(Hx, Hy))
 
 
 def process_data_HSE(fname):
@@ -81,7 +81,7 @@ def process_data_MI(fname, nbins):
     velocity_binned_data = binned_data[:,:,2]
     n_vehicles = velocity_binned_data.shape[1]
 
-    all_entropies = np.zeros(shape=((n_vehicles*(n_vehicles-1))//2, 10))
+    all_entropies = np.zeros(shape=((n_vehicles*(n_vehicles-1))//2, 12))
     row_id = 0
     seed_id = int(fname[fname.find("steps_")+6:-6])
     for v_id_a in range(n_vehicles):
@@ -95,14 +95,81 @@ def process_data_MI(fname, nbins):
 
     return all_entropies, np.mean(all_entropies, axis=0)
 
+def process_data_PI(fname, nbins):
+    # read from disk
+    data = pickle.load(open(fname, "rb"))
+    
+    # reshape to (time, vid, states)
+    data = data.reshape((data.shape[0], -1, 4))
+    
+    # bin to nbins
+    binned_data = (data * nbins).astype(np.int)
+
+    velocity_binned_data = binned_data[:,:,2]
+    n_vehicles = velocity_binned_data.shape[1]
+    n_steps = velocity_binned_data.shape[0]
+
+    all_entropies = np.zeros(shape=(n_vehicles, 11))
+    row_id = 0
+    seed_id = int(fname[fname.find("steps_")+6:-6])
+    for v_id_a in range(n_vehicles):
+        x_series = velocity_binned_data[:n_steps//2, v_id_a]
+        y_series = velocity_binned_data[n_steps//2:, v_id_a]
+        assert len(x_series) == len(y_series)
+        row_dat = investigate(x_series, y_series)
+        all_entropies[row_id, 2:] = row_dat
+        all_entropies[row_id, :2] = (seed_id, v_id_a)
+        row_id += 1
+    return all_entropies, np.mean(all_entropies, axis=0)
+
+def process_data_PI_temporal(fname, nbins):
+    # read from disk
+    data = pickle.load(open(fname, "rb"))
+    
+    # reshape to (time, vid, states)
+    data = data.reshape((data.shape[0], -1, 4))
+    
+    # bin to nbins
+    binned_data = (data * nbins).astype(np.int)
+
+    velocity_binned_data = binned_data[:,:,2]
+    n_vehicles = velocity_binned_data.shape[1]
+    n_steps = velocity_binned_data.shape[0]
+    history_length = 1000
+    data_points = ((n_steps//history_length) - 1)*10
+    step_size = history_length//10
+
+    all_entropies = np.zeros(shape=(data_points, 12))
+    seed_id = int(fname[fname.find("steps_")+6:-6])
+    for t_idx in range(data_points):
+        for v_id_a in range(n_vehicles):
+            x_series = velocity_binned_data[(t_idx)*step_size : (t_idx+1)*step_size, v_id_a]
+            y_series = velocity_binned_data[(t_idx+1)*step_size : (t_idx+2)*step_size, v_id_a]
+            assert len(x_series) == len(y_series)
+            row_dat = investigate(x_series, y_series)
+            all_entropies[t_idx, 3:] = row_dat
+            all_entropies[t_idx, :3] = (seed_id, t_idx*step_size, v_id_a)
+
+    return all_entropies, np.mean(all_entropies, axis=0)
+
+
 def main(client, fnames, nbins):
     results_MI = []
     results_HSE = []
+    results_PI = []
+    results_PI_temporal = []
+
     for fname in fnames:
         results_MI.append(delayed(process_data_MI)(fname, nbins))
 
     for fname in fnames:
         results_HSE.append(delayed(process_data_HSE)(fname))
+    
+    for fname in fnames:
+        results_PI.append(delayed(process_data_PI)(fname, nbins))
+    
+    for fname in fnames:
+        results_PI_temporal.append(delayed(process_data_PI_temporal)(fname, nbins))
     
     merged_data_MI = []
     for fut in client.compute(results_MI):
@@ -114,51 +181,104 @@ def main(client, fnames, nbins):
         res = fut.result()
         merged_data_HSE.append(res)
 
-    return merged_data_MI, merged_data_HSE
+    merged_data_PI = []
+    for fut in client.compute(results_PI):
+        res = fut.result()
+        merged_data_PI.append(res)
+
+    merged_data_PI_temporal = []
+    for fut in client.compute(results_PI_temporal):
+        res = fut.result()
+        merged_data_PI_temporal.append(res)
+
+    return merged_data_MI, merged_data_HSE, merged_data_PI, merged_data_PI_temporal
 
 
 policies = ["Policy", "Policy_Random", "Policy_Random_Network", "Policy_Random_Network2", "Policy_Follow_Leader", "Policy_Boids", "Policy_Simplified_Boids"]
 
 dfsMI = []
 dfsHSE = []
+dfsPI = []
+dfsPIt = []
 for policy in policies:
     fnames = glob.glob("data/{}_10agents_10000steps*".format(policy))
 
-    dat_MI, dat_HSE = main(client, fnames, 10)
+    dat_MI, dat_HSE, dat_PI, dat_PIt = main(client, fnames, 10)
 
     stacked_entropies_MI = np.vstack([d[0] for d in dat_MI])
+    stacked_entropies_PI = np.vstack([d[0] for d in dat_PI])
+    stacked_entropies_PIt = np.vstack([d[0] for d in dat_PIt])
+
     stacked_entropies_HSE = np.vstack( dat_HSE)
 
-    dfMI = pd.DataFrame(stacked_entropies_MI, columns=["Seed", "Vehicle_A", "Vehicle_B", "Hx", "Hy", "Hxy", "Hy_given_x", "Hx_given_y", "MI_xy", "MI_xy_Normalized"])
+    dfMI = pd.DataFrame(stacked_entropies_MI, columns=["Seed", "Vehicle_A", "Vehicle_B", "Min(Hx, Hy)", "Hx+Hy", "Hx", "Hy", "Hxy", "Hy_given_x", "Hx_given_y", "MI_xy", "MI_xy_Normalized"])
+    dfPI = pd.DataFrame(stacked_entropies_PI, columns=["Seed", "Vehicle_A", "Min(Hx, Hy)",  "Hx+Hy", "Hx", "Hy", "Hxy", "Hy_given_x", "Hx_given_y", "PI_xy", "PI_xy_Normalized"])
+    dfPIt = pd.DataFrame(stacked_entropies_PIt, columns=["Seed", "Time", "Vehicle_A", "Min(Hx, Hy)", "Hx+Hy",  "Hx", "Hy", "Hxy", "Hy_given_x", "Hx_given_y", "PI_xy", "PI_xy_Normalized"])
     dfHSE = pd.DataFrame(stacked_entropies_HSE, columns=["Seed", "Time", "HSE"])
+    
     dfMI.insert(0, "Policy", policy)
     dfHSE.insert(0, "Policy", policy)
+    dfPI.insert(0, "Policy", policy)
+    dfPIt.insert(0, "Policy", policy)
+
     dfsMI.append(dfMI)
     dfsHSE.append(dfHSE)
+    dfsPI.append(dfPI)
+    dfsPIt.append(dfPIt)
 
 dfMI = pd.concat(dfsMI)
 dfHSE = pd.concat(dfsHSE)
+dfPI = pd.concat(dfsPI)
+dfPIt = pd.concat(dfsPIt)
 
-ldf = pd.wide_to_long(dfMI, stubnames=[""], i=["Policy", "Seed", "Vehicle_A", "Vehicle_B"], j="Metric", sep="", suffix='[HM]\w+')
-ldf.reset_index(inplace=True)
-ldf.rename(columns={"":"Value"}, inplace=True)
+ldfMI = pd.wide_to_long(dfMI, stubnames=[""], i=["Policy", "Seed", "Vehicle_A", "Vehicle_B"], j="Metric", sep="", suffix='[HM].+')
+ldfMI.reset_index(inplace=True)
+ldfMI.rename(columns={"":"Value"}, inplace=True)
 
-ldfsub = ldf[ldf["Metric"] == "MI_xy_Normalized"]
-
-fig, ax = plt.subplots(figsize=(6,4))
-sns.barplot(y="Policy", x="Value", hue="Metric",ci=95, data=ldfsub, ax=ax)
-plt.savefig("Normalized_MI_Entropies.pdf", bbox_inches='tight')
-plt.show()
+# mutual information
+for metric_of_interest in ["Min(Hx, Hy)", "Hx+Hy", "MI_xy_Normalized", "MI_xy"]:
+    ldfMIsub = ldfMI[ldfMI["Metric"] == metric_of_interest]
+    fig, ax = plt.subplots(figsize=(6,4))
+    sns.barplot(y="Policy", x="Value", hue="Metric",ci=95, data=ldfMIsub, ax=ax)
+    plt.savefig("MI_{}_Entropies.pdf".format(metric_of_interest.replace(" ", "")), bbox_inches='tight')
+    plt.show()
 
 fig, ax = plt.subplots(figsize=(4*7,4))
-sns.barplot(x="Policy", y="Value", hue="Metric",ci=95, data=ldf, ax=ax)
-plt.savefig("MI_Entropies.pdf")
+sns.barplot(x="Policy", y="Value", hue="Metric",ci=95, data=ldfMI, ax=ax)
+plt.savefig("MI_Entropies.pdf", bbox_inches='tight')
 plt.show()
 
+
+# predictive info
+
+for metric_of_interest in ["Min(Hx, Hy)", "Hx+Hy", "PI_xy_Normalized", "PI_xy"]:
+    fig, ax = plt.subplots(figsize=(6,4))
+    sns.barplot(y="Policy", x=metric_of_interest, ci=95, data=dfPI, ax=ax)
+    plt.savefig("PI_{}_Entropies.pdf".format(metric_of_interest.replace(" ", "")), bbox_inches='tight')
+    plt.show()
+
+# fig, ax = plt.subplots(figsize=(4*7,4))
+# sns.barplot(x="Policy", y=["PI_xy", "PI_xy_Normalized"], ci=95, data=dfPI, ax=ax)
+# plt.savefig("PI_Entropies.pdf", bbox_inches='tight')
+# plt.show()
+
+# temporal plots
+for metric_of_interest in ["Min(Hx, Hy)", "Hx+Hy", "PI_xy_Normalized", "PI_xy"]:
+    fig, ax = plt.subplots(figsize=(6, 4))
+    sns.lineplot(x="Time",  y=metric_of_interest, hue="Policy", data=dfPIt, ax=ax)
+    box = ax.get_position()
+    ax.set_position([box.x0, box.y0 + box.height * 0.1,
+                    box.width, box.height * 0.9])
+
+    # Put a legend below current axis
+    ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.15),
+            ncol=2)
+    plt.savefig("PI_{}_Entropies_Temporal.pdf".format(metric_of_interest.replace(" ", "")), bbox_inches='tight')
+    plt.show()
 
 fig, ax = plt.subplots(figsize=(6,4))
 sns.barplot(y="Policy", x="HSE", ci=95, data=dfHSE, ax=ax)
-plt.savefig("HSE_Entropies.pdf")
+plt.savefig("HSE_Entropies.pdf", bbox_inches='tight')
 plt.show()
 
 dfHSETMP = dfHSE[dfHSE["Seed"] == 1]
